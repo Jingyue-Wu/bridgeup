51cfbabf169978cfb2a32eb97eb91810dcf7e184
Jingyue Wu <75918217+Jingyue-Wu@users.noreply.github.com>
2023-08-12 15:40:52 -0400

    Fix broken margins

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -2,12 +2,12 @@ export default function About() {
   return (
     <>
       <div className='lg:m-auto lg:max-w-[40%] m-[5%]'>
-        <h1 className='text-3xl pt-2 my-5'>About</h1>
-
+        <h1 className='text-3xl pt-2 my-5 font-extrabold'>About</h1>
 
         <div className='border-0 my-5 p-5 rounded-[13px] bg-card dark:bg-card_d'>
           <h2 className=' text-sm font-thin text-text2 dark:text-text2_d mb-5'>
-            BridgeUp is a free app that provides the most accurate information on the Welland Canal bridges.
+            BridgeUp is a free app that provides the most accurate information
+            on the Welland Canal bridges.
           </h2>
 
           <h2 className=' text-sm font-thin text-text2 dark:text-text2_d'>
@@ -20,26 +20,33 @@ export default function About() {
           <h1 className='text-xl mb-5'>Download on iOS and Android</h1>
 
           <h2 className=' text-md font-thin mb-5 text-text2 dark:text-text2_d'>
-          <span className="font-bold">To install from Safari:</span> Tap the share icon, scroll down, and tap
-            "Add to Home Screen".
+            <span className='font-bold'>To install from Safari:</span> Tap the
+            share icon, scroll down, and tap "Add to Home Screen".
           </h2>
 
           <h2 className=' text-md font-thin mb-5 text-text2 dark:text-text2_d'>
-            <span className="font-bold">To install from Chrome:</span> Tap the three dots in the top-right of the screen, then tap
-            "Add to Home Screen".
+            <span className='font-bold'>To install from Chrome:</span> Tap the
+            three dots in the top-right of the screen, then tap "Add to Home
+            Screen".
           </h2>
 
-          <h2 className=' text-sm font-thin text-text2 dark:text-text2_d'> The progressive web app will automatically download to your home screen</h2>
+          <h2 className=' text-sm font-thin text-text2 dark:text-text2_d'>
+            {" "}
+            The progressive web app will automatically download to your home
+            screen
+          </h2>
         </div>
 
         <div className='border-0 my-5 p-5 rounded-[13px] bg-card dark:bg-card_d'>
           <h1 className='text-xl mb-5'>Data</h1>
           <h2 className=' text-sm font-thin mb-5 text-text2 dark:text-text2_d'>
-            The data is provided by the St. Lawrence Seaway Management Corporation.
+            The data is provided by the St. Lawrence Seaway Management
+            Corporation.
           </h2>
 
           <h2 className=' text-sm font-thin text-text2 dark:text-text2_d'>
-            Bridge status data is updated every 60 seconds, and closures are updated every 10 minutes.
+            Bridge status data is updated every 60 seconds, and closures are
+            updated every 10 minutes.
           </h2>
         </div>
       </div>
