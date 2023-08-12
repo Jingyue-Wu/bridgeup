0b6a991cf580540efe858604110c091dcdbf09bb
Jingyue Wu <75918217+Jingyue-Wu@users.noreply.github.com>
2023-08-12 16:10:36 -0400

    Update margins

--- src/routes/status.tsx
+++ src/routes/status.tsx
@@ -29,7 +29,7 @@ export default function Status() {
   return (
     <>
       <div className='lg:m-auto lg:max-w-[40%] m-[5%]'>
-        <h1 className='text-3xl pt-2 my-5 font-semibold'>Bridge Status</h1>
+        <h1 className='text-3xl pt-2 my-5'>Bridge Status</h1>
 
 
         {bridges.map((bridge) => (
